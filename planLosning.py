715efd64c0df4ea8f76006846e619a59aefbf93b
chjo0027 <chjo0027@gmail.com>
2016-02-05 22:28:54 +0100

    Update planLosning.py
    tillbaka till main kanske?

--- planLosning.py
+++ planLosning.py
@@ -1,5 +1,6 @@
 import tkinter
 from ljus import send
+from uiMain import UI
 
 class plan:
 
@@ -17,9 +18,11 @@ class plan:
         app.attributes('-fullscreen', True)
         ritning = tkinter.PhotoImage(file="bilder//ritning.gif")
         lampan = tkinter.PhotoImage(file="bilder//lampa2.gif")
+        bakat = tkinter.PhotoImage(file="bilder//exit.gif")
         background_label = tkinter.Label(app, image=ritning)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
+        uiMain = UI
+        
         def lampa1():
             if self.computer_state == "on":
                 send("Computer off")
@@ -32,8 +35,16 @@ class plan:
             else:
                 return
 
+        def back():
+            app.destroy()
+            uiMain.UI()
+            
 
         B1 = tkinter.Button(image=lampan, command=lampa1)
+        #kolla om det funkar
+        B2 = tkinter.Button(hight=20,width=25,image=bakat, command=back)
         B1.place(x=125,y=700)
+        B2.place(x=0,y=0)
+        
         #self.app.after(20,plan)
         app.mainloop()
