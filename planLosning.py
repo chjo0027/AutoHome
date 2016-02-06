56cc1aba2f5e5b244baf0b9a4e2e220da5ef15c2
tedj <tedjohanssondeveloper@gmail.com>
2016-02-06 11:55:36 +0000

    fixa lite kod

--- planLosning.py
+++ planLosning.py
@@ -1,11 +1,12 @@
 import tkinter
 from ljus import send
-from uiMain import UI
+import startsidan
 
 class plan:
 
     def __init__(self):
         self.computer_state = "asd"
+        #self.main = startsidan.startsida()
         '''self.app = tkinter.Tk()
         self.app.geometry("1920x1080")
         self.app.attributes('-fullscreen', True)
@@ -13,6 +14,8 @@ class plan:
         self.lampan = tkinter.PhotoImage(file="bilder//lampa2.gif")'''
 
     def plan(self):
+
+        main = startsidan.startsida()
         app = tkinter.Tk()
         app.geometry("1920x1080")
         app.attributes('-fullscreen', True)
@@ -21,7 +24,6 @@ class plan:
         bakat = tkinter.PhotoImage(file="bilder//exit.gif")
         background_label = tkinter.Label(app, image=ritning)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
-        uiMain = UI
         
         def lampa1():
             if self.computer_state == "on":
@@ -37,14 +39,14 @@ class plan:
 
         def back():
             app.destroy()
-            uiMain.UI()
+            main.sida()
             
 
         B1 = tkinter.Button(image=lampan, command=lampa1)
         #kolla om det funkar
-        B2 = tkinter.Button(hight=20,width=25,image=bakat, command=back)
+        B2 = tkinter.Button(image=bakat, command=back)
         B1.place(x=125,y=700)
-        B2.place(x=0,y=0)
-        
+        B2.place(x=500,y=500)
+
         #self.app.after(20,plan)
         app.mainloop()
